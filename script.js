ee993d9f08b43485fb0099cb6f9b2a3833ee7f79
TARUN DALBERA <dalberatarun1@gmail.con>
2024-09-27 14:35:24 +0530

    weathe image update

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-const location_input=document.getElementById("location_input");
-const search_Btn=document.getElementById("search_Btn");
-const weather_data=document.getElementById("weather_data");
-const data_name=document.getElementById("data_name");
-const data_details=document.getElementById("data_details");
-
-
-
-// const request_url=`http://api.weatherapi.com/v1/current.json?key=1da33a757e3043d98be131231230103&q=$london&aqi=yes`
-// const request_url=`http://api.openweathermap.org/data/2.5/weather?q=${location_input}&appid=${apiKey}&units=metric`
+// const city=document.querySelector(".city");
+// const temperature=document.querySelector(".temp");
+// const humidity=document.querySelector(".humidity");
+// const wind=document.querySelector(".wind");
+// const searchBox=document.querySelector(".search input");
+// const searchBtn=document.querySelector(".search button");
 
 async function getWheather(City_Name){
   const apiKey="267718b976e74b3e638e57314f7971b4"
@@ -15,20 +11,49 @@ async function getWheather(City_Name){
   const response= await fetch(request_url);
   return await response.json();
 }
- 
-search_Btn.addEventListener('click',async()=>{
-  const City_Name=location_input.value;
-  const data=await getWheather(City_Name);
-  console.log(data);
-  if(data.name==undefined){
-    data_name.innerHTML=`City not found`;
-    data_details.innerText=``;
+
+document.querySelector(".search button")
+.addEventListener("click",async()=>{
+  const City_Name=document.querySelector(".search input").value;
+  // console.log(City_Name);
+  if(City_Name!=null) {
+    var data = await getWheather(City_Name);
+    console.log(data);
+    if(data.name==undefined){
+      document.querySelector(".city").innerHTML="city not found"
+    }
+    else{
+      document.querySelector(".city").innerHTML=data.name;
+      document.querySelector(".wind").innerHTML=data.wind.speed+"km\h";
+      document.querySelector(".temp").innerHTML=data.main.temp+"°C";
+      document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
+      document.querySelector(".sky").innerHTML=data.weather[0].main;
+      const weathericon=document.querySelector(".weathericon")
+      // console.log(typeof(data.weather[0].main));
+      switch(data.weather[0].main){
+        case "Clouds":
+          weathericon.src="img/cloudy.png";
+          console.log("Clouds");
+          break;
+        case "Rain":
+          weathericon.src="img/heavy-rain.png";
+          console.log("Rain");
+          break;
+        case "Clear":
+          weathericon.src="img/Weather_fabicon.png";
+          console.log("Clear");
+          break;
+        case "Drizzle":
+          weathericon.src="img/rain.png";
+          console.log("Drizzle");
+          break;
+        default:
+         weathericon.src="";
+
+      }
+      
+    }
   }
-
- else{ data_name.innerHTML=`Weather of ${data.name}`;
-  data_details.innerText=`Sky Conditions: ${data.weather[0].description}
-                       Temperature: ${data.main.temp}C
-                       Humidity:${data.main.humidity}%
-                       Wind Speed: ${data.wind.speed }km/h`;}
-
+  
+  
 })
